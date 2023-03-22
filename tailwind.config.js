31db2f1af6bb70855a64c79c225edd0be188f81c
LaupWing <loc_strict@hotmail.com>
2023-03-22 18:36:37 +0100

    collors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
    content: ["./app/**/*.{js,ts,jsx,tsx}"],
    theme: {
-      extend: {},
+      extend: {
+         colors: {
+            main: {
+               dark: "#141313",
+               gray: "#1D1D1D",
+               light: "#EFEFEF" 
+            },
+            accent: {
+               fire: "#F3A446",
+               bread: "#A06235"
+            }
+         }
+      },
    },
    plugins: [],
 }
