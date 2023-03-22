a634166a888b75839bb18daa514c465ade80fe2d
LaupWing <loc_strict@hotmail.com>
2023-03-22 18:24:45 +0100

    update

--- app/root.tsx
+++ app/root.tsx
@@ -1,65 +1,65 @@
 import {
-  type LinksFunction,
-  type MetaFunction,
-  type LoaderArgs,
-} from '@shopify/remix-oxygen';
+   type LinksFunction,
+   type MetaFunction,
+   type LoaderArgs,
+} from "@shopify/remix-oxygen"
 import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-  useLoaderData,
-} from '@remix-run/react';
-import type {Shop} from '@shopify/hydrogen/storefront-api-types';
-import styles from './styles/app.css';
-import favicon from '../public/favicon.svg';
+   Links,
+   Meta,
+   Outlet,
+   Scripts,
+   ScrollRestoration,
+   useLoaderData,
+} from "@remix-run/react"
+import type { Shop } from "@shopify/hydrogen/storefront-api-types"
+import styles from "./styles/app.css"
+import favicon from "../public/favicon.svg"
 
 export const links: LinksFunction = () => {
-  return [
-    {rel: 'stylesheet', href: styles},
-    {
-      rel: 'preconnect',
-      href: 'https://cdn.shopify.com',
-    },
-    {
-      rel: 'preconnect',
-      href: 'https://shop.app',
-    },
-    {rel: 'icon', type: 'image/svg+xml', href: favicon},
-  ];
-};
+   return [
+      { rel: "stylesheet", href: styles },
+      {
+         rel: "preconnect",
+         href: "https://cdn.shopify.com",
+      },
+      {
+         rel: "preconnect",
+         href: "https://shop.app",
+      },
+      { rel: "icon", type: "image/svg+xml", href: favicon },
+   ]
+}
 
 export const meta: MetaFunction = () => ({
-  charset: 'utf-8',
-  viewport: 'width=device-width,initial-scale=1',
-});
+   charset: "utf-8",
+   viewport: "width=device-width,initial-scale=1",
+})
 
-export async function loader({context}: LoaderArgs) {
-  const layout = await context.storefront.query<{shop: Shop}>(LAYOUT_QUERY);
-  return {layout};
+export async function loader({ context }: LoaderArgs) {
+   const layout = await context.storefront.query<{ shop: Shop }>(LAYOUT_QUERY)
+   return { layout }
 }
 
 export default function App() {
-  const data = useLoaderData<typeof loader>();
+   const data = useLoaderData<typeof loader>()
 
-  const {name} = data.layout.shop;
+   const { name } = data.layout.shop
 
-  return (
-    <html lang="en">
-      <head>
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <h1>Hello, {name}</h1>
-        <p>This is a custom storefront powered by Hydrogen</p>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
-  );
+   return (
+      <html lang="en">
+         <head>
+            <Meta />
+            <Links />
+         </head>
+         <body>
+            <h1>Hello, {name}</h1>
+            <p>This is a custom storefront powered by Hydrogen</p>
+            <Outlet />
+            <ScrollRestoration />
+            <Scripts />
+         </body>
+      </html>
+   )
 }
 
 const LAYOUT_QUERY = `#graphql
@@ -69,4 +69,4 @@ const LAYOUT_QUERY = `#graphql
       description
     }
   }
-`;
+`
