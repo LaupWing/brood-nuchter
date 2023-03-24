8ec0959348ed8980502aa220d7fd1fbe88e5cfa4
LaupWing <loc_strict@hotmail.com>
2023-03-24 17:37:50 +0100

    installed stuff

--- app/components/global/Layout.tsx
+++ app/components/global/Layout.tsx
@@ -1,6 +1,7 @@
 import { FC, PropsWithChildren } from "react"
 import { GiSlicedBread } from "react-icons/gi"
 import { AiOutlineShop } from "react-icons/ai"
+import { useWindowScroll } from "react-use"
 
 const nav_links = [
    {
@@ -35,8 +36,14 @@ export const Layout:FC<PropsWithChildren> = ({
 }
 
 const Header = () => {
+   const { y } = useWindowScroll()
+
    return (
-      <header className="container flex items-center justify-between mx-auto text-white fixed top-0 z-[1000] py-4">
+      <header 
+         className={clsx
+         "container flex items-center justify-between mx-auto text-white fixed top-0 z-[1000] py-4"
+         }
+      >
          <div className="flex items-center">
             <GiSlicedBread size={40} className="mr-1" />
             <div className="flex flex-col font-serif leading-4 text-lg">
