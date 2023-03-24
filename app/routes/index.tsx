7e7071b53ed83195faa76453ff1312f3493aeeb0
LaupWing <loc_strict@hotmail.com>
2023-03-24 17:47:44 +0100

    udpate

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -16,14 +16,17 @@ export default function Index() {
       <>
          <div className="h-screen relative w-full inset-0 flex flex-col">
             <div className="absolute inset-0 bg-main-dark/60 z-50"/>
-            <div className="z-50 my-auto container mx-auto">
+            <div className="z-50 my-auto container mx-auto flex justify-between">
                <div className="font-serif max-w-xs flex flex-col">
                   <h2 className="text-3xl text-white">Lorem ipsum dolor sit, amet consectetur</h2>
                   <p className="text-white text-sm mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis fugiat eveniet obcaecati corporis asperiores molestias eos dignissimos assumenda dolore et explicabo molestiae expedita.</p>
+                  <button className="bg-accent-fire capitalize px-4 py-1 mt-4 shadow font-bold rounded tracking-tight text-main-dark mr-auto">
+                     Shop now
+                  </button>
                </div>
-               <button className="bg-accent-fire capitalize px-4 py-1 mt-4 shadow font-bold rounded tracking-tight text-main-dark">
-                  Shop now
-               </button>
+               <div>
+
+               </div>
             </div>
             <img 
                className="w-full h-full ml-auto object-cover absolute inset-0" 
