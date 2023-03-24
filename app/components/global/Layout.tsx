d536f0700d1ca63efe264b9dd44ac5f420f0e1c2
LaupWing <loc_strict@hotmail.com>
2023-03-24 17:43:35 +0100

    udpate

--- app/components/global/Layout.tsx
+++ app/components/global/Layout.tsx
@@ -2,6 +2,7 @@ import { FC, PropsWithChildren } from "react"
 import { GiSlicedBread } from "react-icons/gi"
 import { AiOutlineShop } from "react-icons/ai"
 import { useWindowScroll } from "react-use"
+import clsx from "clsx"
 
 const nav_links = [
    {
@@ -39,30 +40,31 @@ const Header = () => {
    const { y } = useWindowScroll()
 
    return (
-      <header 
-         className={clsx
-         "container flex items-center justify-between mx-auto text-white fixed top-0 z-[1000] py-4"
-         }
-      >
-         <div className="flex items-center">
-            <GiSlicedBread size={40} className="mr-1" />
-            <div className="flex flex-col font-serif leading-4 text-lg">
-               <h1>Brood</h1>
-               <h1>Nuchter</h1>
+      <div className={clsx(
+         "w-full flex flex-1 fixed top-0 z-[1000] duration-500",
+         y > 50 && "backdrop-blur bg-main-dark/40"
+      )}>
+         <header className="container flex items-center justify-between mx-auto text-white py-4">
+            <div className="flex items-center">
+               <GiSlicedBread size={40} className="mr-1" />
+               <div className="flex flex-col font-serif leading-4 text-lg">
+                  <h1>Brood</h1>
+                  <h1>Nuchter</h1>
+               </div>
             </div>
-         </div>
 
-         <nav className="flex space-x-10 items-center">
-            <ul className="flex space-x-10">
-               {nav_links.map((link, i) =>(
-                  <li 
-                     key={i}
-                     className="tracking-tight"
-                  >{link.name}</li>
-               ))}
-            </ul>
-            <AiOutlineShop size={30}/>
-         </nav>
-      </header>
+            <nav className="flex space-x-10 items-center">
+               <ul className="flex space-x-10">
+                  {nav_links.map((link, i) =>(
+                     <li 
+                        key={i}
+                        className="tracking-tight"
+                     >{link.name}</li>
+                  ))}
+               </ul>
+               <AiOutlineShop size={30}/>
+            </nav>
+         </header>
+      </div>
    )
 }
