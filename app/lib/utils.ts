c97738b7770bbb29dae4f7560f38c69ba658fbd4
LaupWing <loc_strict@hotmail.com>
2023-03-31 09:39:26 +0200

    udpate

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,6 +1,21 @@
-import { Menu } from "@shopify/hydrogen/storefront-api-types"
+import { Menu, MenuItem } from "@shopify/hydrogen/storefront-api-types"
+import { EnhancedMenu, EnhancedMenuItem } from "typings"
 
+const parseItem = () => {
+   return (item:MenuItem): EnhancedMenuItem => {
+      return {
+         ...item,
+         
+      }
+   }
+}
 
-export const parseMenu = (menu: Menu) => {
-
+export const parseMenu = (menu: Menu):(EnhancedMenu|Menu) => {
+   if(!menu.items){
+      console.warn("Invalid menu passed to parseMenu function")
+      return menu
+   }
+   return {
+      ...menu
+   }
 }
