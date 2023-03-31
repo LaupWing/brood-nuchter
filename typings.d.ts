4cc855cff01a6c3edd515ea6855c2af7def341b3
LaupWing <loc_strict@hotmail.com>
2023-03-31 09:32:39 +0200

    EnhancedMenu EnhancedMenuItem

--- typings.d.ts
+++ typings.d.ts
@@ -1,3 +1,5 @@
+import { Menu, MenuItem } from "@shopify/hydrogen/storefront-api-types"
+
 interface BreadType {
    id: number
    name: string 
@@ -6,4 +8,15 @@ interface BreadType {
    image: string 
    weight: number 
    ingredients: string[] 
+}
+
+export interface EnhancedMenu extends Menu {
+   items: EnhancedMenuItem[] 
+}
+
+export interface EnhancedMenuItem extends MenuItem {
+   to: string
+   target: string
+   isExternal?: boolean
+   items: EnhancedMenuItem[]
 }
