c4e171f73cb0512dcf0c5b0419bd009d16013879
LaupWing <loc_strict@hotmail.com>
2023-04-05 10:39:00 +0200

    base cart

---
+++ app/components/cart/Cart.tsx
@@ -0,0 +1,19 @@
+import type { FC } from "react"
+import type { Cart as CartType } from "@shopify/hydrogen/storefront-api-types"
+export type CartLayoutsType = "page" | "drawer"
+
+export const Cart:FC<{
+   layout: CartLayoutsType
+   onClose?: () => void
+   cart: CartType | null
+}> = ({
+   layout,
+   onClose,
+   cart
+}) => {
+   const linesCount = Boolean(cart?.lines?.edges?.length || 0)
+
+   return (
+      <>index</>
+   )
+}
