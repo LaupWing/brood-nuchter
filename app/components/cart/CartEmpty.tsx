516dad0bc7b033600744277d13665fdf4a08ce93
LaupWing <loc_strict@hotmail.com>
2023-04-06 15:44:05 +0200

    udpate

--- app/components/cart/CartEmpty.tsx
+++ app/components/cart/CartEmpty.tsx
@@ -13,7 +13,7 @@ export const CartEmpty:FC<{
    onClose
 }) => {
    const container:Record<CartLayoutsType, string> = {
-      drawer: "content-start gap-4 px-6 pb-8 transition overflow-y-scroll md:gap-12 md:px-12 h-screen-no-nav md:pb-12",
+      drawer: "content-start gap-4 px-6 pb-8 transition overflow-y-auto md:gap-12 md:px-12 h-screen-no-nav md:pb-12",
       page: clsx(
          hidden ? "" : "grid",
          "pb-12 w-full md:items-start gap-4 md:gap-8 lg:gap-12"
@@ -26,9 +26,10 @@ export const CartEmpty:FC<{
          className={container[layout]}
       >
          <section className="grid gap-6">
-            Empty
+            <span>Looks like you haven’t added anything yet, let’s get you started!</span>
+            <button className="bg-accent-fire capitalize px-4 py-1 mt-4 shadow font-bold rounded tracking-tight text-main-dark">Continue shopping</button>
          </section>
-         <section className="grid gap-8">
+         <section className="grid gap-8 pt-16">
             <FeaturedProducts 
                count={4}
                heading="Shop Best Sellers"
