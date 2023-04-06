0446d93ec07db66358e092a834dd7965ce9e5ed9
LaupWing <loc_strict@hotmail.com>
2023-04-06 14:25:30 +0200

    ai

--- app/routes/api/products.tsx
+++ app/routes/api/products.tsx
@@ -2,6 +2,26 @@ import { LoaderArgs } from "@shopify/remix-oxygen"
 
 export const loader = async ({request, context: { storefront }}: LoaderArgs) => {
    const url = new URL(request.url)
-   console.log(url)
+   const searchParams = new URLSearchParams(url.search)
+   
+   const sortKey = searchParams.get("sortKey") ?? "BEST_SELLING"
+   const query = searchParams.get("query") ?? ""
+   let reverse = false
+   let count = 4
+
+   try {
+      const _reverse = searchParams.get("reverse")
+      if(_reverse === "true"){
+         reverse = true
+      }
+   }catch(_){}
+
+   try {
+      const _count = searchParams.get("count")
+      if(typeof _count === "string"){
+         count = parseInt(_count) 
+      }
+   }catch(_){}
+   
    return null
 }
