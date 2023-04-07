e733d10d26c56d1481a8f891fcb6467b5947ffbc
LaupWing <loc_strict@hotmail.com>
2023-04-07 11:33:43 +0200

    CREATE_CART_MUTATION

--- app/routes/cart.tsx
+++ app/routes/cart.tsx
@@ -1,5 +1,5 @@
-import type { Cart as CartType, CartUserError, UserError } from "@shopify/hydrogen/storefront-api-types"
-import { ActionArgs } from "@shopify/remix-oxygen"
+import type { CartInput, CartLineInput, Cart as CartType, CartUserError, UserError } from "@shopify/hydrogen/storefront-api-types"
+import { ActionArgs, AppLoadContext } from "@shopify/remix-oxygen"
 import invariant from "tiny-invariant"
 import { CartAction } from "~/lib/type"
 
@@ -20,6 +20,53 @@ export const action = async ({request, context}: ActionArgs) => {
       cart: CartType
       errors?: CartUserError[] | UserError[]
    }
+
+   switch (cartAction){
+      case CartAction.ADD_TO_CART:
+         const lines = formData.get("lines")
+            ? (JSON.parse(String(formData.get("lines"))) as CartLineInput[])
+            : ([] as CartLineInput[])
+         
+         invariant(lines.length, "No lines added")
+
+         if(!cartId){
+            // result = await cartCreate
+         } else {
+
+         }
+   }
    
    return null
+}
+
+const CREATE_CART_MUTATION = `#graphql
+   mutation($input: CartInput!) {
+      cartCreate(input: $input){
+         cart {
+            id,
+            totalQuantity
+         }
+         errors: userErrors {
+            message
+            field
+            code
+         }
+      }
+   }
+`
+
+const cartCreate = async ({
+   input,
+   storefront
+}:{
+   input: CartInput
+   storefront: AppLoadContext["storefront"]
+}) => {
+   const {} = await storefront.mutate<{
+      cartCreate: {
+         cart: CartType
+         errors: CartUserError[]
+      }
+      errors: UserError[]
+   }>("")
 }
