7f1a249f1c956113e9d320149cd627353c4d1bef
LaupWing <loc_strict@hotmail.com>
2023-04-12 13:05:08 +0200

    update

--- app/components/cart/Cart.tsx
+++ app/components/cart/Cart.tsx
@@ -12,15 +12,15 @@ export const Cart:FC<{
    onClose,
    cart
 }) => {
-   const linesCount = Boolean(cart?.lines?.edges?.length || 0)
+   const isEmpty = Boolean(cart?.lines?.edges?.length || 0)
+   console.log(cart)
 
    return (
       <>
-         <CartEmpty 
-            hidden={linesCount}
+         {isEmpty && <CartEmpty
             onClose={onClose}
             layout={layout}
-         />
+         />}
       </>
    )
 }
--- app/components/cart/CartEmpty.tsx
+++ app/components/cart/CartEmpty.tsx
@@ -4,27 +4,19 @@ import { CartLayoutsType } from "./Cart"
 import { FeaturedProducts } from "../sections"
 
 export const CartEmpty:FC<{
-   hidden: boolean
    layout?: CartLayoutsType
    onClose?: () => void
 }> = ({
-   hidden,
    layout = "drawer",
    onClose
 }) => {
    const container:Record<CartLayoutsType, string> = {
       drawer: "content-start flex flex-col gap-4 px-6 pb-8 transition overflow-y-scroll md:gap-12 md:px-12 md:pb-12",
-      page: clsx(
-         hidden ? "" : "grid",
-         "pb-12 w-full md:items-start gap-4 md:gap-8 lg:gap-12"
-      )
+      page: "pb-12 w-full md:items-start gap-4 md:gap-8 lg:gap-12"
    }
 
    return (
-      <div
-         hidden={hidden}
-         className={container[layout]}
-      >
+      <div className={container[layout]}>
          <section className="grid gap-6">
             <span>Looks like you haven’t added anything yet, let’s get you started!</span>
             <button className="bg-accent-fire capitalize px-4 py-1 mt-4 shadow font-bold rounded tracking-tight text-main-dark">Continue shopping</button>
