20c495e501521a309c85fa93a6ff4725184df390
LaupWing <loc_strict@hotmail.com>
2023-04-12 12:33:47 +0200

    allrighty

--- app/routes/cart.tsx
+++ app/routes/cart.tsx
@@ -1,5 +1,5 @@
 import type { CartInput, CartLineInput, Cart as CartType, CartUserError, UserError } from "@shopify/hydrogen/storefront-api-types"
-import { ActionArgs, AppLoadContext } from "@shopify/remix-oxygen"
+import { ActionArgs, AppLoadContext, json } from "@shopify/remix-oxygen"
 import invariant from "tiny-invariant"
 import { CartAction } from "~/lib/type"
 
@@ -7,11 +7,12 @@ export const action = async ({request, context}: ActionArgs) => {
    const {session, storefront} = context
    const headers = new Headers()
 
-   const [formData, cartId, customerAccessToken] = await Promise.all([
+   const [formData, storeCartId, customerAccessToken] = await Promise.all([
       request.formData(),
       session.get("cartId"),
       session.get("customerAccessToken")
    ])
+   let cartId = storeCartId
    const cartAction = formData.get("cartAction") as CartAction
    invariant(cartAction, "No cartAction defined")
    
@@ -37,11 +38,42 @@ export const action = async ({request, context}: ActionArgs) => {
                storefront
             })
          } else {
-
+            result = await cartAdd({
+               cartId,
+               lines,
+               storefront
+            })
          }
+         cartId = result.cart.id
+         break
+      default:
+         invariant(false, `${cartAction} cart action is not defined`)
    }
    
-   return null
+   session.set("cartId", cartId)
+   headers.set("Set-Cookie", await session.commit())
+
+   const redirectTo = formData.get("redirectTo") ?? null
+
+   if(typeof redirectTo === "string") {
+      status = 303
+      headers.set("Location", redirectTo)
+   }
+
+   const {cart, errors} = result
+   return json(
+      {
+         cart,
+         errors,
+         analytics: {
+            cartId
+         }
+      },
+      {
+         status,
+         headers
+      }
+   )
 }
 
 const CREATE_CART_MUTATION = `#graphql
