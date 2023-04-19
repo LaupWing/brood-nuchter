b4cd8fca9b20ced84b97194524bfb773e7eebe0d
LaupWing <loc_strict@hotmail.com>
2023-04-19 12:57:45 +0200

    UpdateDiscountForm

--- app/components/cart/CartDiscount.tsx
+++ app/components/cart/CartDiscount.tsx
@@ -1,5 +1,7 @@
-import type { FC } from "react"
+import type { FC, PropsWithChildren } from "react"
 import type { Cart as CartType } from "@shopify/hydrogen/storefront-api-types"
+import { useFetcher } from "@remix-run/react"
+import { CartAction } from "~/lib/type"
 
 export const CartDiscount:FC<{
    discountCodes: CartType["discountCodes"]
@@ -12,10 +14,30 @@ export const CartDiscount:FC<{
             <div className="flex items-center justify-between font-medium">
                <dt>Dicount(s)</dt>
                <div className="flex items-center justify-between">
-                  
+
                </div>
             </div>
          </dl>
       </div>
+   )
+}
+
+const UpdateDiscountForm:FC<PropsWithChildren> = ({
+   children
+}) => {
+   const fetcher = useFetcher()
+
+   return (
+      <fetcher.Form 
+         action="/cart"
+         method="post"
+      >
+         <input 
+            type="hidden" 
+            name="cartAction" 
+            value={CartAction.UPDATE_DISCOUNT}
+         />
+         { children }
+      </fetcher.Form>
    )
 }
