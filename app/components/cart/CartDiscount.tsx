4bf6557b695c4a819951e702e77ec9fc953bee09
LaupWing <loc_strict@hotmail.com>
2023-04-19 12:56:22 +0200

    CartDiscount

--- app/components/cart/CartDiscount.tsx
+++ app/components/cart/CartDiscount.tsx
@@ -3,8 +3,19 @@ import type { Cart as CartType } from "@shopify/hydrogen/storefront-api-types"
 
 export const CartDiscount:FC<{
    discountCodes: CartType["discountCodes"]
-}> = () => {
+}> = ({ discountCodes }) => {
+   const codes = discountCodes?.map(({ code }) => code).join(", ") || null
+
    return (
-      <div>CartDiscount</div>
+      <div>
+         <dl className={codes ? "grid" : "hidden"}>
+            <div className="flex items-center justify-between font-medium">
+               <dt>Dicount(s)</dt>
+               <div className="flex items-center justify-between">
+                  
+               </div>
+            </div>
+         </dl>
+      </div>
    )
 }
